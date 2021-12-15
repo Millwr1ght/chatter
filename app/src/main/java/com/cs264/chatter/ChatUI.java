01d2229de998310fa04375e87f71d37a63809cac
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-12-15 13:23:29 -0600

    some additions
    I tried to find a chat app ui tutorial, but all of them involve paying for some software service, not sure if we want to do that

---
+++ app/src/main/java/com/cs264/chatter/ChatUI.java
@@ -0,0 +1,14 @@
+package com.cs264.chatter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_ui);
+    }
+}
